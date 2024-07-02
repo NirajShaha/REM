dad30ce003a7b6de872f502e4ea537ed0e7b702e
Niraj Shaha <86703545+KD3-80177@users.noreply.github.com>
2024-07-02 17:50:36 +0000

    Complete delete user listing

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -124,7 +124,26 @@ export default function Profile() {
     } catch (error) {
       setShowListingError(true);
     }
-  }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -220,6 +239,7 @@ export default function Profile() {
               <div className='flex flex-col item-center'>
                 <button
                   className='text-red-700 uppercase'
+                  onClick={() => handleListingDelete(listing._id)}
                 >
                   Delete
                 </button>
