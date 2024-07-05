8c93c4ce9fa8809aa9d984646020b2602ce6d2a7
Niraj Shaha <86703545+KD3-80177@users.noreply.github.com>
2024-07-05 11:03:49 +0000

    Complete Contact Landlord Function

--- Frontend/src/pages/Listing.jsx
+++ Frontend/src/pages/Listing.jsx
@@ -9,13 +9,12 @@ import {
   FaBath,
   FaBed,
   FaChair,
-  FaMapMarkedAlt,
   FaMapMarkerAlt,
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
-// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -135,6 +134,13 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact  && (
+              <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg 
+              uppercase hover:opacity-95 p-3'
+              >Contact Landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
