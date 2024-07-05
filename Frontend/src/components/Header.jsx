2755382ef9765445942aed7bae902d3753a6124a
Niraj Shaha <86703545+KD3-80177@users.noreply.github.com>
2024-07-05 12:23:30 +0000

    Complete header search form functionality.

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,7 +5,25 @@ import { useEffect, useState } from 'react';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  },[location.search])
 
   return (
     <header className='bg-slate-200 shadow-md'>
@@ -18,12 +36,16 @@ export default function Header() {
         </Link>
         <form
           className='bg-slate-100 p-3 rounded-lg flex items-center'
+          onSubmit={handleSubmit}
         >
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
+
           <button>
             <FaSearch className='text-slate-600' />
           </button>
